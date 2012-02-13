c821df0376cb7a5c17ef5dccc6b5243b2efb45fd
Phokham Nonava <pn@nonava.com>
2012-02-13 14:11:36 +0100

    Refactored example package net.sourceforge.jcpi to jcpi.

--- src/example/java/jcpi/example/MyEngine.java
+++ src/example/java/jcpi/example/MyEngine.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.sourceforge.jcpi.example;
+package jcpi.example;
 
 import jcpi.AbstractCommunication;
 import jcpi.AbstractEngine;
